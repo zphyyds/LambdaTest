ea7c7dca2af24f83618a84cd171d0a3cf715941b
章培鸿 <1547739807@qq.com>
2022-10-24 14:28:16 +0800

    bug-fix 章培鸿

---
+++ src/com/zhang/GitTest.java
@@ -0,0 +1,12 @@
+package com.zhang;
+
+/**
+ * @author zph
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+    }
+}
